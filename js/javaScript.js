7d662d890c4a02006596cabffc174734d00a5a77
javier <espindolajavier2013@gmail.com>
2024-07-31 11:57:19 -0300

    "Added slide technology and portfolio slide functionality, and button home functionality to javaScript.js"

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,22 +1,23 @@
 (() => {
 
+    /*slide technology*/
+
     const copy = document.querySelector(".about__skills-content").cloneNode(true)
 
     document.querySelector(".about__skills").appendChild(copy);
 
-    /*repetir para todos ver como hacerlo xd*/
+    /*slide porfolio*/
 
     let copy2 = [];
 
     document.querySelectorAll(".work__imgCarrousel").forEach(e => copy2.push(e.cloneNode(true)));
 
-    console.log(document.querySelectorAll(".work__imgCarrousel"));
-
     document.querySelectorAll(".work__imgContainerCarrousel").forEach((e, i) => e.appendChild(copy2[i]));
+
+    /* button home */
 
     const scrollUp = () => {
         const scrollUp = document.getElementById('scroll-up')
-        // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
         this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
             : scrollUp.classList.remove('show-scroll')
     }
